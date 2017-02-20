aa2a06700e5fb65b1f9844bd1425079e7c119ff8
wangyushuai <792732447@qq.com>
2017-02-20 13:20:04 +0800

    客户新增显示

--- Application/Home/Controller/KehuController.class.php
+++ Application/Home/Controller/KehuController.class.php
@@ -5,7 +5,27 @@ use Think\Controller;
 
 class KehuController extends Controller {
     public function kehu(){
-  
+  		$a=M('yewuziduan');
+  		$map['zd_yewu']="客户";
+  		$map['zd_yh']="1";//这里通过查询获得
+  		$sql=$a->where($map)->field('zd_data')->select();
+  		$json=$sql['0']["zd_data"];
+
+  		$a_arr=json_decode($json,true);
+
+  		$need=array();
+
+		foreach($a_arr as $k=>$v)
+		{
+			if($v['qy']==1)
+			{
+			$need[$k]['id']=$v['id'];
+			$need[$k]['name']=$v['name'];
+			$need[$k]['bt']=$v['bt'];
+			}
+	
+		}
+		$this->assign('kehu',$need);
         $this->display();
     }
 
