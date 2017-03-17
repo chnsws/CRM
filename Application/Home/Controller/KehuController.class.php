10d339cf0d18ba809ba790536b066e322a0abdfa
wangyushuai <792732447@qq.com>
2017-03-17 17:27:43 +0800

    客户文件上传及详细信息

--- Application/Home/Controller/KehuController.class.php
+++ Application/Home/Controller/KehuController.class.php
@@ -134,22 +134,28 @@ class KehuController extends Controller {
 		
 		foreach($ronghe as $r_k=>$r_v)
 		{
-			$table.="<tr>";
+			$table.="<tr id='tr".$r_v['0']."'>";
 			foreach($r_v as $k=>$v)
 			{
 		$id=$r_v['0'];
+		//var_dump($k);exit;
+		//echo "<pre>";
+		//	var_dump($r_v['0']);exit;
+				if($k=='zdy0')
+					$xs123="<a href='kehumingcheng'><input type='text' width='20px' name='{$k}' id='{$id}' value='{$v}' readonly='true' style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
+					</a>";
 				
+				else
+					$xs123="<input type='text' width='20px' name='{$k}' id='{$id}' class='bianji' value='{$v}' onblur=''  style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
+					<i class='fa fa-pencil' aria-hidden='true'>	</i>";
 				$table.="<td name='$k'>
-
-
-
-					<input type='text' width='20px' name='{$k}' id='{$id}' class='bianji' value='{$v}' onblur=''  style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
-					<i class='fa fa-pencil' aria-hidden='true'>	</i>
-
-						
+					$xs123
+				
+		
 				</td>";
 			}
 			$table.="</tr>";
+			
 			
 		}
 
@@ -176,7 +182,7 @@ class KehuController extends Controller {
 		    }
 
 
-
+ 
 
 
 		public function index(){//测试
@@ -274,7 +280,7 @@ class KehuController extends Controller {
 	
 			foreach($ronghe as $r_k=>$r_v)
 				{
-					$table.="<tr>";
+					$table.="<tr id='tr".$k."'>";
 					foreach($r_v as $k=>$v)
 					{
 				$id=$r_v['0'];
@@ -283,7 +289,7 @@ class KehuController extends Controller {
 
 
 
-							<input type='text' width='20px' name='{$k}' id='{$id}' class='bianji' value='{$v}' onblur=''  style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
+							<input type='text' width='20px' name='$k' id='$id' class='bianji' value='{$v}' onblur=''  style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
 							<i class='fa fa-pencil' aria-hidden='true'>	</i>
 
 								
@@ -292,13 +298,105 @@ class KehuController extends Controller {
 					$table.="</tr>";
 					
 				}
+					$hidden=json_encode($ronghe,true);
+					
 
 					echo $table;
-		
+					
 
 
 
 		}
+		public function shaixuan(){
+
+				$kehu=M('kh');                             //显示客户所需字段data
+						$kehu=$kehu->select();
+						//echo"<pre>";
+
+						foreach($kehu as $k=>$v){
+							$nachu[$k]=json_decode($v['kh_data'],true);
+						}
+					
+						foreach($nachu as $k=>$v){
+						 			foreach($v as $k1=>$v1){
+						 				foreach ($ywcs_sql_json as $key=>$val){		 				
+						 					if($k1==$val['id']){
+						 						$v[$k1]=$val[$v1];						
+						 					} 		 					
+						 				}
+						 			}
+						 			$guanlianw[]=$v;
+						 		}
+
+					foreach($kehu as $k=>$val){
+						$valav=array_merge($guanlianw[$k],$val);
+							echo "<pre>";
+				print_r($k);exit;
+						$dantiao=$valav['kh_id'];//获取到id
+						unset($valav['kh_id']); 
+						unset($valav['kh_data']); 
+						array_unshift($valav,$dantiao); //整理好的单条信息
+						
+							$ronghe[]=$valav;	 //多条融合	
+					}
+				echo "<pre>";
+				print_r($ronghe);exit;
+
+		}
+		public function kehumingcheng(){
+			$sql=M('file');
+			$sql_select=$sql->select();
+			$this->assign('sql',$sql_select);
+			$this->display();
+		}
+		public function delete(){
+			$sql['id']=$_GET['id'];
+			 $sql_delete=M('file');
+       		$sql_file_select=$sql_delete->where($sql)->delete();
+       		if($sql_file_select){
+       				$this->success("删除成功",U('Kehu/kehumingcheng'));
+       		}else{
+       			 	$this->error("删除失败");
+       			 }
+		}
+		public function upload(){
+			    $upload = new \Think\Upload();// 实例化上传类
+    			$upload->maxSize   =     3145728 ;// 设置附件上传大小
+   				$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg','txt','pptx','xls');// 设置附件上传类型
+    			$upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+   				$upload->autoSub = false;
+   				$upload->hash = false;
+    		// 上传文件 
+   				 $info   =   $upload->upload();
+    				if(!$info) {// 上传错误提示错误信息
+        		$this->error($upload->getError());
+    				}// 上传成功
+    					    foreach($info as $file){
+       						$save_name= 'Uploads/'.$file['savename'];//获取报存路径
+       						$save_oldname=$file['name'];//原始吗，
+       						$save_size=$file['size'] *'0.0009766';//大小
+       						$sql=substr($save_size,0,3).'kb';//换算
+    							}
+    			 $data['name_id']=12;
+    			 $data['sc_data']= date("Y-m-d h:i:s");
+    			 $data['fujian_name']=$save_oldname;
+    			 $data['lujing']=$save_name;
+    			 $data['big']=$sql;
+       			 $data['beizhu']=$_POST['wenbenyu'];
+       			 
+       			 $sql_file=M('file');
+       			 $sql_file_select=$sql_file->add($data);
+       			 if($sql_file_select)
+       			 {
+       			 	$this->success("上传成功",U('Kehu/kehumingcheng'));
+       			 	
+       			 }else{
+       			 	$this->error("上传失败");
+       			 }
+		}
+	
+
+
 
     
 }
