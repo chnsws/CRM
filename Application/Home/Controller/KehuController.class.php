049d0de94e21d64226e0e17fbf250ba122439bfa
wangyushuai <792732447@qq.com>
2017-03-22 17:58:15 +0800

    日志跟进

--- Application/Home/Controller/KehuController.class.php
+++ Application/Home/Controller/KehuController.class.php
@@ -146,7 +146,7 @@ class KehuController extends Controller {
 		//echo "<pre>";
 		//	var_dump($r_v['0']);exit;
 				if($k=='zdy0')
-					$xs123="<a href='kehumingcheng/id/$v/fuzeren/$a_fuzeren'><input type='text' width='20px' name='{$k}' id='{$id}' value='{$v}' readonly='true' style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
+					$xs123="<a href='kehumingcheng/id/$v/fuzeren/$a_fuzeren/id1/$id/kh_id/$id'><input type='text' width='20px' name='{$k}' id='{$id}' value='{$v}' readonly='true' style='border-left:0px;border-top:0px;border-right:0px;border-bottom:1px '>
 					</a>";
 				
 				else
@@ -173,35 +173,100 @@ class KehuController extends Controller {
 
 
 		public function add(){
-		    $data['kh_data']=$_GET['id'];  	
+		    $data['kh_data']=$_GET['id'];
+		    	$a_arr=json_decode( $data['kh_data'],true);
+	
 			$shi=M('kh');
 			$sql=$shi->add($data);
+
+				$loginIp=$_SERVER['REMOTE_ADDR'];//IP 
+           	 	$sysbroinfo=getSysBro();//一维数组 sys->系统 bro->浏览器
+            	$addressArr=getCity($nowip);//登录地点
+            	$loginDidianStr=$addressArr["country"].$addressArr["region"].$addressArr["city"];
+
+		   		$id=$shi->where($data)->field('kh_id')->find();	
+		   		$rz=M('rz');
+		 		$rz_map['rz_type']=1;//这个1是操作日志类型  死的
+		 		$rz_map['rz_mode']=2;
+		 		$rz_map['rz_object']=$id['kh_id'];//客户名称ID
+				$rz_map['rz_bz']="新增了客户".$a_arr['zdy0'];
+				$rz_map['rz_time']=time();
+				$rz_map['rz_user']=cookie('user_id');
+				$rz_map['rz_ip']=$loginIp;//ip
+				$rz_map['rz_place']=$loginDidianStr;//登录地点
+				$rz_map['rz_sb']=$sysbroinfo['sys'].'/'.$sysbroinfo['bro'];//ip
+				$fid=cookie('user_fid')=='0'?cookie('user_id'):cookie('user_fid');//获取所属用户（所属公司）
+				$rz_map['rz_yh']=$fid;
+				$rz_sql=$rz->add($rz_map);//查'
 			if($sql){
 				echo "ok";
-		
+			
 			}else{
 				echo "no";
-		}
-		       
+		     
 		    }
 
-
+}
  
 
 
-		public function index(){//测试
+		public function index(){//这才是修改
+
 			$bianji_id['kh_id']= $_GET['bianji_id'];//81
 
 			$bianji_name= $_GET['bianji_name'];//zdy2  fuzeren
 			$bianji_val= $_GET['bianji_val'];//修改内容
 			$sql=substr($bianji_name,0,3);
+			$kehus=M('kh'); 
 
-			
-			
-		$kehus=M('kh'); 
-
-
+		
 		if($sql=='zdy'){
+
+
+
+			$ywzd=M('yewuziduan');              //只是为了获取  zd0   的中文名字放备注中
+				$yw_cs['zd_yewu']="客户";
+ 				$yw_cs['zd_yh']=1;
+				$ywzd_sql=$ywzd->where($yw_cs)->find();
+				$sql_json=json_decode($ywzd_sql['zd_data'],true);
+				foreach($sql_json as $k=>$v){
+					if($v['id']==$bianji_name){
+						$name_rz=$v['name'];
+					}
+				}                                    //获取完了
+
+				
+				$map_rz['kh_id']=$bianji_id['kh_id'];  //这里获取修改之前的值 日志记录用
+				$kh_old_val=$kehus->where(array($map_rz))->field('kh_data')->find();
+				$sql_json_rz=json_decode($kh_old_val['kh_data'],true);
+				foreach($sql_json_rz as $krz=>$vrz){
+					if($krz==$bianji_name){
+						$b_rz=$vrz;
+					}
+				}
+				$loginIp=$_SERVER['REMOTE_ADDR'];//IP 
+           	 	$sysbroinfo=getSysBro();//一维数组 sys->系统 bro->浏览器
+            	$addressArr=getCity($nowip);//登录地点
+            	$loginDidianStr=$addressArr["country"].$addressArr["region"].$addressArr["city"];
+
+		   		$id=$bianji_id['kh_id'];	
+		   		$rz=M('rz');
+		 		$rz_map['rz_type']=1;//这个1是操作日志类型  死的
+		 		$rz_map['rz_mode']=2;
+		 		$rz_map['rz_object']=$id;//客户名称ID
+				$rz_map['rz_bz']="把".$name_rz.'的'.$b_rz."改为".$bianji_val;
+				$rz_map['rz_user']=cookie('user_id');
+				$rz_map['rz_time']=time();
+				$rz_map['rz_ip']=$loginIp;//ip
+				$rz_map['rz_place']=$loginDidianStr;//登录地点
+				$rz_map['rz_sb']=$sysbroinfo['sys'].'/'.$sysbroinfo['bro'];//ip
+				$fid=cookie('user_fid')=='0'?cookie('user_id'):cookie('user_fid');//获取所属用户（所属公司）
+				$rz_map['rz_yh']=$fid;
+				$rz_sql=$rz->add($rz_map);//查'
+
+
+
+
 			$sql_bianji=$kehus->where($bianji_id)->find();
 
 			$sql_json=json_decode($sql_bianji['kh_data'],true);
@@ -335,7 +400,7 @@ class KehuController extends Controller {
 					foreach($kehu as $k=>$val){
 						$valav=array_merge($guanlianw[$k],$val);
 							echo "<pre>";
-				print_r($k);exit;
+							print_r($k);exit;
 						$dantiao=$valav['kh_id'];//获取到id
 						unset($valav['kh_id']); 
 						unset($valav['kh_data']); 
@@ -343,19 +408,75 @@ class KehuController extends Controller {
 						
 							$ronghe[]=$valav;	 //多条融合	
 					}
-				echo "<pre>";
-				print_r($ronghe);exit;
+				//echo "<pre>";
+				//print_r($ronghe);exit;
 
 		}
 		public function kehumingcheng(){
-			$a_id=$_GET['id'];
+			$a_id=$_GET['id'];//客户名称
+			$kh_id=$_GET['kh_id'];//客户ID=$_GET['kh_id'];//客户ID
+	$fuzeren=$_GET['fuzeren'];
+			$id=$_GET['id1'];//客户名称ID
+			$kh=M('kh');
+			$kh_map['kh_id']=$kh_id;
+			$sql_kh=$kh->where($kh_map)->field('kh_data')->find();
+			$sql_json=json_decode($sql_kh['kh_data'],true);
+			$kh_type=$sql_json['zdy1'];//客户类型canshu1
+			$kh_phone=$sql_json['zdy2'];//客户电话
+
 			
-			$fuzeren=$_GET['fuzeren'];
+			$ywcs=M('ywcs');                 //获取ywcs表中的 数据
+	 		$yw_cs['ywcs_yw']="客户";
+	 		$yw_cs['ywcs_yh']=1;
+	 		$ywcs_sql=$ywcs->where($yw_cs)->field('ywcs_data')->find();
+	 		$ywcs_sql_json=json_decode($ywcs_sql['ywcs_data'],true);
+	 		foreach($ywcs_sql_json as $k=>$v){
+	 			if($v['id']=='zdy1'){
+	 				$kh_type2=$v[$kh_type];
+	 			}
+	 		}
+
+	 		$this->assign('kh_phone',$kh_phone);//客户电话
+	 		$this->assign('kh_type2',$kh_type2);//客户类型
+			
+	 		//写跟进查询
+	 		$rz=M('rz');
+	 		$rz_map['rz_type']=1;//这个1是操作日志类型  死的
+	 		$rz_map['rz_mode']=2;//这个是模块ID也是死的
+			$rz_map['rz_object']=$kh_id;//客户名称ID
+			$rz_sql=$rz->where(array($rz_map))->field('rz_bz,rz_time,rz_user')->order("rz_time desc")->select();//查询出日志记录、
+//echo "<pre>";
+//var_dump($rz_sql);exit;
+
+			$rz_user=M('user');
+			$user_sql=$rz_user->field('user_id,user_name')->select();
+			$kh_map1['user_id']=$fuzeren;
+			$kh_name=$rz_user->where($kh_map1)->field("user_name")->find();
+			
+			foreach($rz_sql as $k=>$v){//跟进循环的数据
+				foreach($user_sql as $k1 =>$v1){
+					if($v['rz_user']==$v1['user_id'])
+					{
+						$v['rz_user']= $v1['user_name'];
+						$v['rz_time']=date("Y-m-d H:i:s",$v['rz_time']);
+					}
+				}
+				$ko[]=$v;
+			}
+			$this->assign('genjin',$ko);
+			
+		
 			$sql=M('file');
 			$sql_select=$sql->select();
 			$this->assign('sql',$sql_select);
 			$this->assign('a_id',$a_id);
-			$this->assign('fuzeren',$fuzeren);
+			$this->assign('fuzeren',$kh_name);//上面是 客户全景  和附件
+
+			$hetong=M('hetong');
+				$tiaojian='"zdy1":"'.$id.'"';
+				
+			$sql_hetong=$hetong->query("select * from crm_hetong where ht_data like '%$tiaojian%'");
+
 			$this->display();
 		}
 		public function delete(){
