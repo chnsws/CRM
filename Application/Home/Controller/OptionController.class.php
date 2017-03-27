19df97457538017d46d58d4fe3914c1f455ee951
yinwence <1375629208@qq.com>
2017-03-27 10:11:23 +0800

    完善业绩目标模块功能

--- Application/Home/Controller/OptionController.class.php
+++ Application/Home/Controller/OptionController.class.php
@@ -277,6 +277,317 @@ class OptionController extends Controller {
 	}
 	//业绩目标
 	public function yejimubiao(){
+		if(cookie("islogin")!='1')
+		{
+			echo "<script>window.location='".$_GET['root_dir']."/index.php/Home/Login'</script>";
+			die();
+		}
+		$mbname=array(
+            '1'=>'赢单商机金额',
+            '2'=>'赢单商机数',
+            '3'=>'合同回款金额',
+            '4'=>'合同金额',
+            '5'=>'合同数',
+            '6'=>'产品销量',
+            '7'=>'产品销售额',
+            '8'=>'产品分类销量',
+            '9'=>'产品分类销售额'
+        );
+		$cpidarr=array('6','7');
+		$cptypearr=array('6','7');
+		$fid=cookie('user_fid')=='0'?cookie('user_id'):cookie('user_fid');//获取所属用户（所属公司）
+		$yjmbbase=M("yjmb");
+		
+		
+		$yjmbarr=$yjmbbase->query("select * from crm_yjmb where yjmb_yh='$fid' order by yjmb_nd desc");
+		foreach($yjmbarr as $v)
+		{
+			if($v['yjmb_type']=='6'||$v['yjmb_type']=='7')
+			{
+				$cpinstr.="'".$v['yjmb_type_more']."',";//产品
+			}
+			if($v['yjmb_type']=='8'||$v['yjmb_type']=='9')
+			{
+				$cptypeinstr.="'".$v['yjmb_type_more']."',";//产品分类
+			}
+		}
+		if($cpinstr!='')
+		{
+			$cpinstr=substr($cpinstr,0,-1);
+			$cpbase=M("chanpin");
+			$cparr=$cpbase->query("select cp_id,cp_data from crm_chanpin where cp_yh='$fid' and cp_id in ($cpinstr)");
+			foreach($cparr as $v)
+			{
+				$decodecp=json_decode($v['cp_data'],true);
+				$cpname[$v['cp_id']]=$decodecp['zdy0'];
+			}
+		}
+		if($cptypeinstr!='')
+		{
+			$cptypeinstr=substr($cptypeinstr,0,-1);
+			$cptypebase=M("chanpinfenlei");
+			$cptypearr=$cptypebase->query("select cpfl_id,cpfl_name from crm_chanpinfenlei where cpfl_company='$fid' and cpfl_id in ($cptypeinstr)");
+			foreach($cptypearr as $v)
+			{
+				$cptypename[$v['cpfl_id']]=$v['cpfl_name'];
+			}
+		}
+		$yjmorebase=M("yjmb_user");
+		$alluseryjarr=$yjmorebase->query("select * from crm_yjmb_user left join crm_user on yjm_uid=user_id where yjm_fid='$fid' and user_del='0' ");
+		foreach($alluseryjarr as $v)
+		{
+			$sum='0';
+			foreach($v as $k=>$val)
+			{
+				if(substr($k,0,5)=='yjm_m')
+					$sum=$val+$sum;
+			}
+			$zongmubiao[$v['yjm_yid']]=$zongmubiao[$v['yjm_yid']]+$sum;
+		}
+		foreach($yjmbarr as $v)
+		{
+			if($v['yjmb_type']=='6'||$v['yjmb_type']=='7')
+			{
+				$relname='('.$cpname[$v['yjmb_type_more']].')';
+			}
+			else if($v['yjmb_type']=='8'||$v['yjmb_type']=='9')
+			{
+				$relname='('.$cptypename[$v['yjmb_type_more']].')';
+			}
+			else
+			{
+				$relname='';
+			}
+			$yjtable.="<tr><td>".$v['yjmb_nd']."</td><td><a href='".$_GET['root_dir']."/index.php/Home/Option/yejimubiao_more?yjid=".$v['yjmb_id']."'>".$mbname[$v['yjmb_type']].$relname."</a></td><td>".$zongmubiao[$v['yjmb_id']]."</td><td><a href='".$_GET['root_dir']."/index.php/Home/Option/yejimubiao_more?yjid=".$v['yjmb_id']."'>查看</a><a onclick='yjcopy(".$v['yjmb_nd'].",".$v['yjmb_id'].",".$v['yjmb_type'].",".$v['yjmb_type_more'].")'>复制</a><a onclick='yjdel(".$v['yjmb_id'].")'>删除</a></td></tr>";
+		}
+		$this->assign("yjtable",$yjtable);
+		$this->display();
+	}
+	//业绩目标详细
+	public function yejimubiao_more(){
+		if(cookie("islogin")!='1')
+		{
+			echo "<script>window.location='".$_GET['root_dir']."/index.php/Home/Login'</script>";
+			die();
+		}
+		$moreyjid=addslashes($_GET['yjid']);
+		$fid=cookie('user_fid')=='0'?cookie('user_id'):cookie('user_fid');//获取所属用户（所属公司）
+		$yjbase=M("yjmb");
+		$yjuserbase=M("yjmb_user");
+		$yjarr=$yjbase->query("select * from crm_yjmb where yjmb_id='$moreyjid' limit 1");
+		if($yjarr[0]['yjmb_type']=='1'||$yjarr[0]['yjmb_type']=='3'||$yjarr[0]['yjmb_type']=='4'||$yjarr[0]['yjmb_type']=='7'||$yjarr[0]['yjmb_type']=='9')
+		{
+			$danwei='￥';
+			$danwei2='￥';
+			$listdanwei='￥';
+			$ismoney='1';
+		}
+		else
+		{
+			$danwei='总数量：';
+			$danwei2='';
+			$listdanwei='';
+			$ismoney='0';
+		}
+		//业绩类型名称
+		$mbname=array(
+            '1'=>'赢单商机金额',
+            '2'=>'赢单商机数',
+            '3'=>'合同回款金额',
+            '4'=>'合同金额',
+            '5'=>'合同数',
+            '6'=>'产品销量',
+            '7'=>'产品销售额',
+            '8'=>'产品分类销量',
+            '9'=>'产品分类销售额'
+        );
+		if($yjarr[0]['yjmb_type']=='6'||$yjarr[0]['yjmb_type']=='7')
+		{
+			$cpbase=M("chanpin");
+			$cpnamearr=$cpbase->query("select cp_data from crm_chanpin where cp_id='".$yjarr[0]['yjmb_type_more']."'");
+			$cpnamearr=json_decode($cpnamearr[0]['cp_data'],true);
+			$cpname=$mbname[$yjarr[0]['yjmb_type']].'('.$cpnamearr['zdy0'].')';//产品
+		}
+		else if($yjarr[0]['yjmb_type']=='8'||$yjarr[0]['yjmb_type']=='9')
+		{
+			$cptypebase=M("chanpinfenlei");
+			$cpnamearr=$cptypebase->query("select cpfl_name from crm_chanpinfenlei where cpfl_id='".$yjarr[0]['yjmb_type_more']."'");
+			$cpname=$mbname[$yjarr[0]['yjmb_type']].'('.$cpnamearr[0]['cpfl_name'].')';//产品
+		}
+		else
+		{
+			$cpname=$mbname[$yjarr[0]['yjmb_type']];
+		}
+		//构造右边表格
+		$yjuserarr=$yjuserbase->query("select * from crm_yjmb_user left join crm_user on yjm_uid=user_id where yjm_yid='$moreyjid' and user_del='0' ");
+		//每个人和每个部门的业绩，组成业绩表格
+		foreach($yjuserarr as $v)
+		{
+			$sumyj='0';//每个人的总业绩
+			//总量
+			foreach($v as $k=>$vv)
+			{
+				if(substr($k,0,5)=='yjm_m')
+				{
+					$sumyj+=$vv;
+					$companysum+=$vv;//整个公司的总业绩
+				}		
+			}		
+			//每个部门的总业绩
+			$bmsumarr[$v['user_zhu_bid']]+=$sumyj;
+			//构造用户表
+			$tablestr.="<tr id='tr".$v['yjm_id']."'><td>".$v['user_name']."</td><td class='sumyj'>".$sumyj."</td><td class='change-text'>".$v['yjm_m1']."</td><td class='change-text'>".$v['yjm_m2']."</td><td class='change-text'>".$v['yjm_m3']."</td><td class='change-text'>".$v['yjm_m4']."</td><td class='change-text'>".$v['yjm_m5']."</td><td class='change-text'>".$v['yjm_m6']."</td><td class='change-text'>".$v['yjm_m7']."</td><td class='change-text'>".$v['yjm_m8']."</td><td class='change-text'>".$v['yjm_m9']."</td><td class='change-text'>".$v['yjm_m10']."</td><td class='change-text'>".$v['yjm_m11']."</td><td class='change-text'>".$v['yjm_m12']."</td></tr>";
+		}
+		//echo "<pre>";
+		//print_r($yjuserarr);
+		//构造左边部门
+		//部门表操作
+		$bumenbase=M("department");
+		$bumenArr=$bumenbase->query("select * from crm_department where bm_company='$fid' ");
+		foreach($bumenArr as $bumenArrKey=>$bumenArrVal)//格式化部门分级
+		{
+			$bumenNewArr[$bumenArrVal['bm_id']]=array("bm_name"=>$bumenArrVal['bm_name'],"bm_fid"=>$bumenArrVal['bm_fid']);
+		}
+		//部门遍历排序
+		foreach($bumenNewArr as $bmNewKey=>$bmNewVal)
+		{
+			$bumenFname[$bmNewKey]=$bumenNewArr[$bmNewVal['bm_fid']]['bm_name'];
+			if($bmNewVal['bm_fid']=='0')//顶级部门
+			{
+				$bmLvArr[$bmNewKey]['bm_name']=$bmNewVal['bm_name'];
+			}
+			else
+			{
+				if($bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']=='0')
+				{
+					$bmLvArr[$bmNewVal['bm_fid']]["lv2"][$bmNewKey]["bm_name"]=$bmNewVal['bm_name'];
+				}
+				else
+				{
+					if($bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']=='0')
+					{
+						$bmLvArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['lv2'][$bmNewVal['bm_fid']]['lv3'][$bmNewKey]["bm_name"]=$bmNewVal['bm_name'];
+					}
+					else
+					{
+						if($bumenNewArr[$bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']]['bm_fid']=='0')
+						{
+							$bmLvArr[$bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']]['lv2'][$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['lv3'][$bmNewVal['bm_fid']]['lv4'][$bmNewKey]["bm_name"]=$bmNewVal['bm_name'];
+						
+						}
+						else
+						{
+							if($bumenNewArr[$bumenNewArr[$bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']]['bm_fid']]['bm_fid']=='0')
+							{
+								$bmLvArr[$bumenNewArr[$bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']]['bm_fid']]['lv2'][$bumenNewArr[$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['bm_fid']]['lv3'][$bumenNewArr[$bmNewVal['bm_fid']]['bm_fid']]['lv4'][$bmNewVal['bm_fid']]['lv5'][$bmNewKey]["bm_name"]=$bmNewVal['bm_name'];
+							}
+						}
+					}
+				}
+			}
+		}
+		//总销量分级
+		foreach($bmLvArr as $k=>$v)
+		{
+			$bmsum=$bmsumarr[$k]==''?'0':$bmsumarr[$k];
+			$bmallarr[$k]+=$bmsum;
+			if(count($v['lv2'])>0)
+			{
+				foreach($v['lv2'] as $lv2k=>$lv2v)
+				{
+					$bmsum=$bmsumarr[$lv2k]==''?'0':$bmsumarr[$lv2k];
+					$bmallarr[$k]+=$bmsum;
+					$bmallarr[$lv2k]+=$bmsum;
+					if(count($lv2v['lv3'])>0)
+					{
+						foreach($lv2v['lv3'] as $lv3k=>$lv3v)
+						{
+							$bmsum=$bmsumarr[$lv3k]==''?'0':$bmsumarr[$lv3k];
+							$bmallarr[$k]+=$bmsum;
+							$bmallarr[$lv2k]+=$bmsum;
+							$bmallarr[$lv3k]+=$bmsum;
+							if(count($lv3v['lv4'])>0)
+							{
+								foreach($lv3v['lv4'] as $lv4k=>$lv4v)
+								{
+									$bmsum=$bmsumarr[$lv4k]==''?'0':$bmsumarr[$lv4k];
+									$bmallarr[$k]+=$bmsum;
+									$bmallarr[$lv2k]+=$bmsum;
+									$bmallarr[$lv3k]+=$bmsum;
+									$bmallarr[$lv4k]+=$bmsum;
+									if(count($lv4v['lv5'])>0)
+									{
+										foreach($lv4v['lv5'] as $lv5k=>$lv5v)
+										{
+											$bmsum=$bmsumarr[$lv5k]==''?'0':$bmsumarr[$lv5k];
+											$bmallarr[$k]+=$bmsum;
+											$bmallarr[$lv2k]+=$bmsum;
+											$bmallarr[$lv3k]+=$bmsum;
+											$bmallarr[$lv4k]+=$bmsum;
+											$bmallarr[$lv5k]+=$bmsum;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		//echo "<pre>";
+		//print_r($bmallarr);
+		//部门排序结束 $bmLvArr:部门分级数组
+		//echo "<pre>";
+		//print_r($bmLvArr);
+		//echo number_format(floor('22'),2);
+		//生成部门结构HTML
+		foreach($bmLvArr as $k=>$v)
+		{
+			$bmList.="<li class='lv1 lv-on' id='id".$k."' value='1' name='1'><i class='fa fa-folder-open' aria-hidden='true'></i><span class='left-li'>".$v['bm_name']."</span><span class='right-span'>".$listdanwei.$this->yjmbdanwei($bmallarr[$k],$ismoney)."</span></li>";
+			if(count($v['lv2'])>0)
+			{
+				foreach($v['lv2'] as $lv2k=>$lv2v)
+				{
+					$bmList.="<li class='lv2 lv-on lv1".$k."' id='id".$lv2k."' value='1' name='2'><i class='fa fa-folder-open' aria-hidden='true'></i><span class='left-li'>".$lv2v['bm_name']."</span><span class='right-span'>".$listdanwei.$this->yjmbdanwei($bmallarr[$lv2k],$ismoney)."</span></li>";
+					if(count($lv2v['lv3'])>0)
+					{
+						foreach($lv2v['lv3'] as $lv3k=>$lv3v)
+						{
+							$bmList.="<li class='lv3 lv-on lv2".$lv2k." lv1".$k."' id='id".$lv3k."' value='1' name='3'><i class='fa fa-folder-open' aria-hidden='true'></i><span class='left-li'>".$lv3v['bm_name']."</span><span class='right-span'>".$listdanwei.$this->yjmbdanwei($bmallarr[$lv3k],$ismoney)."</span></li>";
+							if(count($lv3v['lv4'])>0)
+							{
+								foreach($lv3v['lv4'] as $lv4k=>$lv4v)
+								{
+									$bmList.="<li class='lv4 lv-on lv3".$lv3k." lv2".$lv2k." lv1".$k."' id='id".$lv4k."' value='1' name='4'><i class='fa fa-folder-open' aria-hidden='true'></i><span class='left-li'>".$lv4v['bm_name']."</span><span class='right-span'>".$listdanwei.$this->yjmbdanwei($bmallarr[$lv4k],$ismoney)."</span></li>";
+							
+									if(count($lv4v['lv5'])>0)
+									{
+										foreach($lv4v['lv5'] as $lv5k=>$lv5v)
+										{
+											$bmList.="<li class='lv5 lv-on lv4".$lv4k." lv3".$lv3k." lv2".$lv2k." lv1".$k."' id='id".$lv5k."' value='1' name='5'><i class='fa fa-folder-open' aria-hidden='true'></i><span class='left-li'>".$lv5v['bm_name']."</span><span class='right-span'>".$listdanwei.$this->yjmbdanwei($bmallarr[$lv5k],$ismoney)."</span></li>";
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		//公司名称查询
+		$gsbase=M("gongsixinxi");
+		$gsname=$gsbase->query("select gsxx_name from crm_gongsixinxi where gsxx_yh='$fid' limit 1");
+		//变量映射
+		$this->assign("pageid",$moreyjid);
+		$this->assign("pagetype",$yjarr[0]['yjmb_type']);
+		$this->assign("danwei",$danwei);//右上角返回按钮附近的单位
+		$this->assign("danwei2",$danwei2);
+		$this->assign("companysum",$this->yjmbdanwei($companysum,$ismoney));//全公司的总量
+		$this->assign("gsname",$gsname[0]['gsxx_name']);//公司名称
+		$this->assign("bmlist",$bmList);//部门列表
+		$this->assign("tablestr",$tablestr);//用户列表
+		$this->assign("biaoti",$yjarr[0]['yjmb_nd'].'年度业绩目标');//标题
+		$this->assign("cpname",$cpname);//业绩类型
 		$this->display();
 	}
 	//工作报告
@@ -378,6 +689,18 @@ class OptionController extends Controller {
 		$this->assign("caozuostr",$caozuostr);
 		$this->assign("usersel",$usersel);
 		$this->display();
+	}
+	function yjmbdanwei($str,$val)
+	{
+		if($val=='0')
+		{
+			$returnstr=$str;
+		}
+		else
+		{
+			$returnstr=number_format($str,2);
+		}
+		return $returnstr;
 	}
 }
 
