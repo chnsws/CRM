b1de33a598a280725f89db981afd2a753072da7e
yinwence <1375629208@qq.com>
2017-06-29 12:15:01 +0800

    uikit日期插件汉化

--- Public/uikit/js/components/datepicker.js
+++ Public/uikit/js/components/datepicker.js
@@ -27,8 +27,8 @@
             mobile: false,
             weekstart: 1,
             i18n: {
-                months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
-                weekdays      : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+                months        : ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+                weekdays      : ['日','一','二','三','四','五','六']
             },
             format: "YYYY-MM-DD",
             offsettop: 5,
