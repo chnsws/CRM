0928e57cb2e899b7584e0b1e8e6d3e8ed2c67fe8
wangyushuai <792732447@qq.com>
2017-09-04 10:35:20 +0800

    312

---
+++ Application/Home/Controller/EmptyController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+
+class EmptyController extends Controller {
+	 public function _empty(){ // 定义空操作（_empty）方法
+      	$this->display();
+        }
+	
+}
