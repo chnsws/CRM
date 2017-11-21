f3b9ee392e618fba76849247432d77003e574d30
yinwence <1375629208@qq.com>
2017-11-21 18:05:45 +0800

    增加客户公海导航栏

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,7 +7,7 @@ class IndexController extends DBController {
     public function index(){
         parent::is_login2(1);
         $fid=parent::get_fid();
-        $navs='[{"title":"工作台","icon":"fa-table","href":"","spread":false},{"title":"线索","icon":"fa-search","href":"index.php/Home/Xiansuo/index","spread":false},{"title":"客户","icon":"fa-group","href":"index.php/Home/Kehu/kehu","spread":false},{"title":"联系人","icon":"fa-address-book","href":"index.php/Home/Lianxiren/lianxiren","spread":false},{"title":"商机","icon":"fa-money","href":"index.php/Home/Shangji/shangji","spread":false},{"title":"合同","icon":"fa-file","href":"index.php/Home/Hetong/hetong","spread":false},{"title":"产品","icon":"fa-book","href":"index.php/Home/Cpfl/cpfl_index","spread":false},{"title":"审批","icon":"fa-search","href":"index.php/Home/Shenpi/shenpi","spread":false},{"title":"报表中心","icon":"fa-align-left","href":"index.php/Home/Report/index","spread":false},{"title":"问题反馈","icon":"fa-envelope","href":"index.php/Home/Fankui/index","spread":false},{"title":"收起","icon":"fa-chevron-left","href":"#","spread":false}]';
+        $navs='[{"title":"工作台","icon":"fa-table","href":"","spread":false},{"title":"线索","icon":"fa-search","href":"index.php/Home/Xiansuo/index","spread":false},{"title":"客户","icon":"fa-group","href":"index.php/Home/Kehu/kehu","spread":false},{"title":"客户公海","icon":"fa-group","href":"index.php/Home/Gonghai/gonghai","spread":false},{"title":"联系人","icon":"fa-address-book","href":"index.php/Home/Lianxiren/lianxiren","spread":false},{"title":"商机","icon":"fa-money","href":"index.php/Home/Shangji/shangji","spread":false},{"title":"合同","icon":"fa-file","href":"index.php/Home/Hetong/hetong","spread":false},{"title":"产品","icon":"fa-book","href":"index.php/Home/Cpfl/cpfl_index","spread":false},{"title":"审批","icon":"fa-search","href":"index.php/Home/Shenpi/shenpi","spread":false},{"title":"报表中心","icon":"fa-align-left","href":"index.php/Home/Report/index","spread":false},{"title":"问题反馈","icon":"fa-envelope","href":"index.php/Home/Fankui/index","spread":false},{"title":"收起","icon":"fa-chevron-left","href":"#","spread":false}]';
         $newnavjson=$navs;
         if(cookie("user_quanxian")!='1')
         {
@@ -26,12 +26,13 @@ class IndexController extends DBController {
             $nav_index=array(
                 "1"=>"qx_xs_open",
                 "2"=>"qx_kh_open",
-                "3"=>"qx_lxr_open",
-                "4"=>"qx_sj_open",
-                "5"=>"qx_ht_open",
-                "6"=>"qx_cp_open",
-                "7"=>"qx_sp_open",
-                "8"=>"qx_bb_open"
+                "3"=>"qx_khgh_open",
+                "4"=>"qx_lxr_open",
+                "5"=>"qx_sj_open",
+                "6"=>"qx_ht_open",
+                "7"=>"qx_cp_open",
+                "8"=>"qx_sp_open",
+                "9"=>"qx_bb_open"
             );
             $navarr=json_decode($navs,true);
             foreach($navarr as $k=>$v)
