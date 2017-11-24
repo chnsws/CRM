a001f263aedd12ac810f72beb1774ff5dd36576e
wangyushuai <792732447@qq.com>
2017-11-24 20:15:22 +0800

    23

--- Application/Home/Controller/KehuController.class.php
+++ Application/Home/Controller/KehuController.class.php
@@ -1327,7 +1327,21 @@ class KehuController extends Controller {
 			$kh=M('kh');
 			$kh_map['kh_id']=$kh_id;
 			$sql_kh=$kh->where($kh_map)->find();
-			
+			$qxa=0;
+			foreach($new_array1 as $k=>$v)
+			{
+				if($sql_kh['kh_fz']==$v)
+				{
+					$qxa++;
+				}
+			}
+		
+			if($qxa<1)
+			{
+				echo "<script> alert('您没有查看此客户权限~');history.go(-1); 
+				 
+				</script>";die;
+			}
 			$sql_json=json_decode($sql_kh['kh_data'],true);
 		
 			$kh_type=$sql_json['zdy1'];//客户类型canshu1
