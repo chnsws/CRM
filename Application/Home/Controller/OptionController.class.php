34999efcffa2aba82bd43ef66f063034af34b2ec
yinwence <1375629208@qq.com>
2018-03-02 15:39:31 +0800

    增加合同模板设置

--- Application/Home/Controller/OptionController.class.php
+++ Application/Home/Controller/OptionController.class.php
@@ -11,7 +11,7 @@ class OptionController extends DBController {
 		$fid=parent::get_fid();
 
 		//$nav='[{"title":"设置中心","icon":"fa-table","href":"","spread":true},{"title":"系统设置","icon":"fa-search","href":"","spread":true,"children":[{"title":"部门和用户设置","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/bumenyonghu"},{"title":"角色和权限设置","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/juesequanxian"},{"title":"公司信息","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/companyinfo"},{"title":"公告管理","icon":"&#xe609;","href":"'.__ROOT__.'/index.php/Home/Option/gonggaoguanli"}]},{"title":"业务设置","icon":"fa-group","href":"","spread":true,"children":[{"title":"业绩目标","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/yejimubiao"},{"title":"客户公海","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/kehugonghai"}]},{"title":"自定义设置","icon":"fa-address-book","href":"","spread":true,"children":[{"title":"自定义业务字段","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Yewuziduan/index"},{"title":"自定义业务参数","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Yewucanshu/index"},{"title":"自定义审批","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/shenpi"},{"title":"产品筛选设置","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/shaixuan"}]},{"title":"查询","icon":"fa-money","href":"","spread":true,"children":[{"title":"日志查询","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/rizhi"}]}]';
-		$nav='[{"title":"设置中心","icon":"fa-table","href":"","spread":true},{"title":"系统设置","icon":"fa-search","href":"","spread":true,"children":[{"title":"部门和用户设置","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/bumenyonghu"},{"title":"角色和权限设置","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/juesequanxian"},{"title":"公司信息","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/companyinfo"},{"title":"公告管理","icon":"&#xe609;","href":"'.__ROOT__.'/index.php/Home/Option/gonggaoguanli"}]},{"title":"业务设置","icon":"fa-group","href":"","spread":true,"children":[{"title":"业绩目标","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/yejimubiao"},{"title":"客户公海","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/kehugonghai"}]},{"title":"自定义设置","icon":"fa-address-book","href":"","spread":true,"children":[{"title":"自定义业务字段","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Yewuziduan/index"},{"title":"自定义业务参数","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Yewucanshu/index"},{"title":"自定义审批","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/shenpi"}]},{"title":"查询","icon":"fa-money","href":"","spread":true,"children":[{"title":"日志查询","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/rizhi"}]}]';
+		$nav='[{"title":"设置中心","icon":"fa-table","href":"","spread":true},{"title":"系统设置","icon":"fa-search","href":"","spread":true,"children":[{"title":"部门和用户设置","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/bumenyonghu"},{"title":"角色和权限设置","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/juesequanxian"},{"title":"公司信息","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/companyinfo"},{"title":"公告管理","icon":"&#xe609;","href":"'.__ROOT__.'/index.php/Home/Option/gonggaoguanli"}]},{"title":"业务设置","icon":"fa-group","href":"","spread":true,"children":[{"title":"业绩目标","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/yejimubiao"},{"title":"客户公海","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/kehugonghai"}]},{"title":"自定义设置","icon":"fa-address-book","href":"","spread":true,"children":[{"title":"自定义业务字段","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Yewuziduan/index"},{"title":"自定义业务参数","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Yewucanshu/index"},{"title":"自定义合同模板","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Hetongmbzdy/zdy"},{"title":"自定义审批","icon":"&#xe63c;","href":"'.__ROOT__.'/index.php/Home/Option/shenpi"}]},{"title":"查询","icon":"fa-money","href":"","spread":true,"children":[{"title":"日志查询","icon":"&#xe641;","href":"'.__ROOT__.'/index.php/Home/Option/rizhi"}]}]';
 
 		$navarr=json_decode($nav,true);
 		//parent::rr($navarr);
@@ -28,6 +28,7 @@ class OptionController extends DBController {
 			$qx_sys_khgh=cookie("qx_sys_khgh");
 			$qx_sys_ywzd=cookie("qx_sys_ywzd");
 			$qx_sys_ywcs=cookie("qx_sys_ywcs");
+			$qx_sys_ywcs=cookie("qx_sys_htmb");
 			$qx_sys_sp=cookie("qx_sys_sp");
 			$qx_sys_sx=cookie("qx_sys_sx");
 			$qx_sys_rz=cookie("qx_sys_rz");
@@ -495,7 +496,6 @@ class OptionController extends DBController {
 	public function gonggaomore()
 	{
 		parent::is_login2(2);
-		parent::have_qx2("qx_sys_gggl");
 		$ggid=addslashes($_GET['ggid']);
 		if($ggid=='')
 		{
